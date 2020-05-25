62deed336ea1c06c1d5a2a52819995191b33b3dd
DAQ121 <2829025551@qq.com>
2020-05-25 20:13:40 +0800

    单链表

---
+++ List/LinkList.java
@@ -0,0 +1,110 @@
+package com.daq.list;
+
+@SuppressWarnings("all")
+public class LinkList {
+
+    //结点类
+    private class ListNode {
+        private Object data;//数据域
+        private ListNode next = null;//指针域
+        ListNode() {//初始化头结点
+            data = null;
+        }
+        ListNode(Object data) {
+            this.data = data;
+        }
+    }
+
+    private ListNode head;//头结点
+    private ListNode temp;//临时结点
+
+    //初始化链表，生成一个无数据的头节点
+    LinkList() {
+        head = new ListNode();
+    }
+
+    /**
+     * 增加节点
+     * @param data
+     */
+    public void addNode(Object data) {
+        ListNode node = new ListNode(data);
+        temp = head;
+        //链表不为空时
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+        temp.next = node;
+    }
+
+    /**
+     * 返回链表的长度
+     * @return
+     */
+    public int getLength()
+    {
+        temp = head;
+        int length = 0;
+        while (temp.next!=null)
+        {
+            length++;
+            temp = temp.next;
+        }
+        return length;
+    }
+
+    /**
+     * 增加节点到指定的位置
+     * @param index
+     * @param data
+     */
+    public void addNodeByIndex(int index, Object data) {
+        if (index < 1 || index > getLength() + 1) {
+            System.out.println("插入的位置不合法。");
+            return;
+        }
+        int count = 1; //记录遍历的位置
+        ListNode node = new ListNode(data);
+        temp = head;
+        while (temp.next != null) {
+            if (index == count++) {
+                node.next = temp.next;
+                temp.next = node;
+                return;
+            }
+            temp = temp.next;
+        }
+    }
+
+    /**
+     * 删除指定位置的节点
+     * @param index
+     */
+    public void deleteByIndex(int index) {
+        if (index < 1 || index > getLength()) {
+            System.out.println("插入的位置不合法。");
+            return;
+        }
+        int count = 1;//记录位置
+        temp = head;
+        while (temp.next != null) {
+            if (index == count++) {
+                temp.next = temp.next.next;
+                return;
+            }
+            temp = temp.next;
+        }
+    }
+
+    /**
+     * 从头到尾打印节点
+     */
+    public void printListFromHead() {
+        temp = head;
+        while (temp.next != null) {
+            System.out.print("{" + temp.next.data + "}");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+}
