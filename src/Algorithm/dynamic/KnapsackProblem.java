61418ca7f08884569bdeeb079901b5601f380904
Josie <49462890+DAQ121@users.noreply.github.com>
2020-08-06 10:26:21 +0800

    动态规划

--- src/Algorithm/dynamic/KnapsackProblem.java
+++ src/Algorithm/dynamic/KnapsackProblem.java
@@ -4,43 +4,42 @@ public class KnapsackProblem {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int[] w = {1, 4, 3};//��Ʒ������
-		int[] val = {1500, 3000, 2000}; //��Ʒ�ļ�ֵ ����val[i] ����ǰ�潲��v[i]
-		int m = 4; //����������
-		int n = val.length; //��Ʒ�ĸ���
+		int[] w = {1, 4, 3};//物品的重量
+		int[] val = {1500, 3000, 2000}; //物品的价值 这里val[i] 就是前面讲的v[i]
+		int m = 4; //背包的容量
+		int n = val.length; //物品的个数
 		
 		
-		
-		//������ά���飬
-		//v[i][j] ��ʾ��ǰi����Ʒ���ܹ�װ������Ϊj�ı����е�����ֵ
+		//创建二维数组，
+		//v[i][j] 表示在前i个物品中能够装入容量为j的背包中的最大价值
 		int[][] v = new int[n+1][m+1];
-		//Ϊ�˼�¼������Ʒ����������Ƕ�һ����ά����
+		//为了记录放入商品的情况，我们定一个二维数组
 		int[][] path = new int[n+1][m+1];
 		
-		//��ʼ����һ�к͵�һ��, �����ڱ������У����Բ�ȥ��������ΪĬ�Ͼ���0
+		//初始化第一行和第一列, 这里在本程序中，可以不去处理，因为默认就是0
 		for(int i = 0; i < v.length; i++) {
-			v[i][0] = 0; //����һ������Ϊ0
+			v[i][0] = 0; //将第一列设置为0
 		}
 		for(int i=0; i < v[0].length; i++) {
-			v[0][i] = 0; //����һ������0
+			v[0][i] = 0; //将第一行设置0
 		}
 		
 		
-		//����ǰ��õ���ʽ����̬�滮����
-		for(int i = 1; i < v.length; i++) { //��������һ�� i�Ǵ�1��ʼ��
-			for(int j=1; j < v[0].length; j++) {//��������һ��, j�Ǵ�1��ʼ��
-				//��ʽ
-				if(w[i-1]> j) { // ��Ϊ���ǳ���i �Ǵ�1��ʼ�ģ����ԭ����ʽ�е� w[i] �޸ĳ� w[i-1]
+		//根据前面得到公式来动态规划处理
+		for(int i = 1; i < v.length; i++) { //不处理第一行 i是从1开始的
+			for(int j=1; j < v[0].length; j++) {//不处理第一列, j是从1开始的
+				//公式
+				if(w[i-1]> j) { // 因为我们程序i 是从1开始的，因此原来公式中的 w[i] 修改成 w[i-1]
 					v[i][j]=v[i-1][j];
 				} else {
-					//˵��:
-					//��Ϊ���ǵ�i ��1��ʼ�ģ� ��˹�ʽ��Ҫ������
+					//说明:
+					//因为我们的i 从1开始的， 因此公式需要调整成
 					//v[i][j]=Math.max(v[i-1][j], val[i-1]+v[i-1][j-w[i-1]]);
 					//v[i][j] = Math.max(v[i - 1][j], val[i - 1] + v[i - 1][j - w[i - 1]]);
-					//Ϊ�˼�¼��Ʒ��ŵ���������������ǲ���ֱ�ӵ�ʹ������Ĺ�ʽ����Ҫʹ��if-else�����ֹ�ʽ
+					//为了记录商品存放到背包的情况，我们不能直接的使用上面的公式，需要使用if-else来体现公式
 					if(v[i - 1][j] < val[i - 1] + v[i - 1][j - w[i - 1]]) {
 						v[i][j] = val[i - 1] + v[i - 1][j - w[i - 1]];
-						//�ѵ�ǰ�������¼��path
+						//把当前的情况记录到path
 						path[i][j] = 1;
 					} else {
 						v[i][j] = v[i - 1][j];
@@ -50,7 +49,7 @@ public class KnapsackProblem {
 			}
 		}
 		
-		//���һ��v ����Ŀǰ�����
+		//输出一下v 看看目前的情况
 		for(int i =0; i < v.length;i++) {
 			for(int j = 0; j < v[i].length;j++) {
 				System.out.print(v[i][j] + " ");
@@ -59,22 +58,22 @@ public class KnapsackProblem {
 		}
 		
 		System.out.println("============================");
-		//�����������Ƿ������Щ��Ʒ
-		//����path, �������������еķ���������õ�, ��ʵ����ֻ��Ҫ���ķ���
+		//输出最后我们是放入的哪些商品
+		//遍历path, 这样输出会把所有的放入情况都得到, 其实我们只需要最后的放入
 //		for(int i = 0; i < path.length; i++) {
 //			for(int j=0; j < path[i].length; j++) {
 //				if(path[i][j] == 1) {
-//					System.out.printf("��%d����Ʒ���뵽����\n", i);
+//					System.out.printf("第%d个商品放入到背包\n", i);
 //				}
 //			}
 //		}
 		
-		//���Խ�
-		int i = path.length - 1; //�е�����±�
-		int j = path[0].length - 1;  //�е�����±�
-		while(i > 0 && j > 0 ) { //��path�����ʼ��
+		//动脑筋
+		int i = path.length - 1; //行的最大下标
+		int j = path[0].length - 1;  //列的最大下标
+		while(i > 0 && j > 0 ) { //从path的最后开始找
 			if(path[i][j] == 1) {
-				System.out.printf("��%d����Ʒ���뵽����\n", i); 
+				System.out.printf("第%d个商品放入到背包\n", i); 
 				j -= w[i-1]; //w[i-1]
 			}
 			i--;
